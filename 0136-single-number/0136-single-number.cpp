e99509a2b7c4da4b117d3274f6b6fe6dbb05fa65
Sayantan Chaulia <sayantanastudent@gmail.com>
2026-09-25 20:12:59 +0530

    Time: 0 ms (100%), Space: 20.6 MB (83.74%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int XoR = 0;
+        for(int i = 0;i<nums.size();i++){
+            XoR = XoR ^ nums[i];
+        }
+        return XoR;
+    }
+};
