ae7e3b842c4d332a49fe5fa6d0e59084e7ab62c8
Sayantan Chaulia <sayantanastudent@gmail.com>
2026-09-26 21:33:31 +0530

    Time: 0 ms (100%), Space: 50.2 MB (99.29%) - LeetHub

---
+++ 4061-minimum-queen-moves-to-reach-target/4061-minimum-queen-moves-to-reach-target.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minQueenMoves(vector<int>& source, vector<int>& target) {
+        int sr = source[0], sc = source[1];
+        int tr = target[0], tc = target[1];
+
+        if(sr == tr && sc == tc){return 0;}
+        else if (sr == tr || sc==tc || abs(sr -tr )==abs(sc-tc)){return 1;}
+        else return 2;
+    }
+};
